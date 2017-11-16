0a6d1c4b26ce8aa4b43066043f3420f103751e6b
Eduardo Elias Saleh <eduardo@lab15.io>
2017-11-16 16:54:04 +0100

    1.110

--- src/platform/svalidated/builder.component.d.ts
+++
@@ -1,5 +0,0 @@
-import { FormBuilder } from "@angular/forms/src/form_builder";
-
-
-export class SVFormBuilder extends FormBuilder {    
-}
--- src/platform/svalidated/group.component.d.ts
+++
@@ -1,5 +0,0 @@
-import { FormGroup } from "@angular/forms/src/model";
-
-export class SVFormGroup extends FormGroup {
-    
-}
